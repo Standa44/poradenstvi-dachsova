35268ce3b8d8b544b55b361c30b6418d35fff8bb
Stanislav Tomek <tomek.standa@seznam.cz>
2025-03-04 16:39:04 +0100

    header component changes

--- src/components/LayoutComponents/HeaderComponent.js
+++ src/components/LayoutComponents/HeaderComponent.js
@@ -1,90 +1,84 @@
 import {
-    Box,
-    Flex,
-    HStack,
-    IconButton,
-    Link,
-    Drawer,
-    DrawerBody,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-    useDisclosure,
-    VStack,
-  } from "@chakra-ui/react";
-  import { HamburgerIcon } from "@chakra-ui/icons";
-  
-  const HeaderComponent = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-  
-    return (
-      <Box as="header" bg="white" shadow="md" px={4} >
-        <Flex
-          h={16}
-          alignItems="center"
-          justifyContent="space-between"
-          maxW="1200px"
-          mx="auto" position="sticky" top="0" zIndex="20"
-        >
-          {/* Logo */}
-          <Box fontWeight="bold" fontSize="lg">
-            Daňové poradenství Dachsová
+  Box, Flex, IconButton, Drawer, DrawerBody, DrawerOverlay, DrawerContent, DrawerCloseButton,
+  useDisclosure, VStack, Heading, Image
+} from "@chakra-ui/react";
+import Link from 'next/link';
+import { HamburgerIcon } from "@chakra-ui/icons";
+
+const HeaderComponent = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Box as="header" bg="white" shadow="md" px={4}>
+      <Flex h={20} alignItems="center"
+        justifyContent="space-between"
+        maxW="1200px" mx="auto" position="sticky"
+        top="0" zIndex="20"
+      >
+        <Flex alignItems="center">
+          <Image src="images/komora-danovych-poradcu-logo.png" w={{ base: "15%", md: "10%" }} />
+          <Box fontWeight="bold" fontSize="lg" ml={4}>
+            <Heading size="md">Ing. Jana Dachsová</Heading>
+            <Heading size="xs" color="gray">Daňová poradkyně</Heading>
           </Box>
-  
-          {/* Desktop Navigation */}
-          <HStack as="nav" spacing={6} display={{ base: "none", md: "flex" }}>
-            <Link href="#domu" fontWeight="medium">
-              Úvodní stránka
-            </Link>
-            <Link href="#o-me" fontWeight="medium">
-              O mě
-            </Link>
-            <Link href="#sluzby" fontWeight="medium">
-              Naše služby
-            </Link>
-            <Link href="#kontakt" fontWeight="medium">
-              Kontakty
-            </Link>
-          </HStack>
-  
-          {/* Mobile Menu Icon */}
-          <IconButton 
-            size="md"
-            icon={<HamburgerIcon />}
-            aria-label="Open menu"
-            display={{ md: "none" }}
-            onClick={onOpen}
-          />
         </Flex>
-  
-        {/* Mobile Drawer Menu */}
-        <Drawer
-          isOpen={isOpen}
-          placement="right"
-          onClose={onClose}
-          size="xs" // Nastavuje šířku - xs je zhruba 3/4 stránky
-        >
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerBody>
-              <VStack as="nav" spacing={4} mt={10}>
-                <Link href="#home" onClick={onClose} fontSize="lg">
-                  Domů
-                </Link>
-                <Link href="#services" onClick={onClose} fontSize="lg">
-                  Služby
-                </Link>
-                <Link href="#contact" onClick={onClose} fontSize="lg">
-                  Kontakt
-                </Link>
-              </VStack>
-            </DrawerBody>
-          </DrawerContent>
-        </Drawer>
-      </Box>
-    );
-  };
-  
-  export default HeaderComponent;
-  
+
+        {/* Desktop Navigation */}
+        <Flex as="nav" gap={8} display={{ base: "none", md: "flex" }} whiteSpace="nowrap">
+          <Link href="/" fontWeight="medium" mx={2}>
+            Úvodní stránka
+          </Link>
+          <Link href="sluzby" fontWeight="medium" mx={2}>
+            Služby
+          </Link>
+          <Link href="cenik" fontWeight="medium" mx={2}>
+            Ceník
+          </Link>
+          <Link href="kontakt" fontWeight="medium" mx={2}>
+            Kontakt
+          </Link>
+        </Flex>
+
+        {/* Mobile Menu Icon */}
+        <IconButton
+          size="md"
+          icon={<HamburgerIcon />}
+          aria-label="Open menu"
+          display={{ md: "none" }}
+          onClick={onOpen}
+        />
+      </Flex>
+
+      {/* Mobile Drawer Menu */}
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        size="xs" // Nastavuje šířku - xs je zhruba 3/4 stránky
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerBody>
+            <VStack as="nav" spacing={4} mt={10}>
+              <Link href="/" passHref onClick={onClose} fontSize="lg">
+                Domů
+              </Link>
+              <Link href="/sluzby" passHref onClick={onClose} fontSize="lg">
+                Služby
+              </Link>
+              <Link href="/cenik" passHref onClick={onClose} fontSize="lg">
+                Ceník
+              </Link>
+              <Link href="/kontakt" passHref onClick={onClose} fontSize="lg">
+                Kontakt
+              </Link>
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </Box>
+  );
+};
+
+export default HeaderComponent;

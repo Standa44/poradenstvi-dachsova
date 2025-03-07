6914c2dcc1e269e7003227daf6a1101236c7f59e
Stanislav Tomek <tomek.standa@seznam.cz>
2025-03-07 10:51:34 +0100

    social links

--- src/components/LayoutComponents/Footer.js
+++ src/components/LayoutComponents/Footer.js
@@ -1,37 +1,30 @@
-import { Box, Flex, Text, Icon, Link, VStack } from "@chakra-ui/react";
+import { Box, Flex, Text, Link, VStack } from "@chakra-ui/react";
 import { FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
+import SocialLinks from "./SocialLinks";
 
 const Footer = () => {
   const getCurrentYear = () => new Date().getFullYear();
+  const socialLinks = [
+    { icon: FaFacebook, url: "https://facebook.com" },
+    { icon: FaLinkedin, url: "https://linkedin.com" },
+    { icon: FaTwitter, url: "https://twitter.com" }
+  ];
+  
 
   return (
     <Box bg="gray.800" color="white" py={6} px={4}>
       <Flex justify="space-between" flexDir={{ base: "column", md: "row" }} align="center">
-        {/* Logo a copyright */}
         <VStack align="start">
           <Text fontSize="lg" fontWeight="bold">Ing. Jana Dachsová</Text>
           <Text fontSize="sm">© {getCurrentYear()} Všechna práva vyhrazena.</Text>
         </VStack>
 
-        {/* Rychlé odkazy */}
         <VStack align="start" spacing={1} mt={{ base: 4, md: 0 }}>
           <Link href="#sluzby" fontSize="sm">Služby</Link>
           <Link href="#o-me" fontSize="sm">O nás</Link>
           <Link href="#kontakt" fontSize="sm">Kontakt</Link>
         </VStack>
-
-        {/* Sociální sítě */}
-        <Flex mt={{ base: 4, md: 0 }}>
-          <Link href="https://facebook.com" isExternal mx={2}>
-            <Icon as={FaFacebook} boxSize={6} />
-          </Link>
-          <Link href="https://linkedin.com" isExternal mx={2}>
-            <Icon as={FaLinkedin} boxSize={6} />
-          </Link>
-          <Link href="https://twitter.com" isExternal mx={2}>
-            <Icon as={FaTwitter} boxSize={6} />
-          </Link>
-        </Flex>
+        <SocialLinks links={socialLinks} />
       </Flex>
     </Box>
   );

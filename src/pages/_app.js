860b71ae95e3e40a748b27ec6d423999e5db3062
Stanislav Tomek <tomek.standa@seznam.cz>
2025-04-03 13:20:51 +0200

    import of Layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import "@/styles/globals.css";
-import Layout from '@/components/LayoutComponents/Layout';
+import Layout from '@/components/Layout/Layout';
 import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
@@ -12,6 +12,7 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="keywords" content="daňové poradenství Liberec, účetnictví Liberec, daňový poradce, účetní služby, mzdová agenda, daňová přiznání, optimalizace daní, firemní účetnictví, daňová evidence, účetnictví pro podnikatele, finanční poradenství, daňová kontrola, DPH, mzdové účetnictví, Liberec daně, Frýdlant daně" />
         <meta name="author" content="Stanislav Tomek" />
+        <meta name="robots" content="all"></meta>
         <meta property="og:title" content="Daňové poradenství Dachsová" />
         <meta property="og:description" content="Profesionální daňové poradenství pro podnikatele a firmy." />
         <meta property="og:image" content="/preview.jpg" />

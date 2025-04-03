22d7dbe3478f90ed7f6d632d0a14e0d08c0cf5f9
Stanislav Tomek <tomek.standa@seznam.cz>
2025-04-03 11:18:07 +0200

    created new contact component

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -1,23 +1,12 @@
-import ContactForm from '@/components/Index/ContactForm'
-import ContactInfo from '@/components/Index/ContactInfo'
-import { Box, Heading, Text } from '@chakra-ui/react'
+import ContactForm from '@/components/Kontakt/ContactForm'
+import ContactInfoSection from '@/components/Kontakt/ContactInfoSection'
 import React from 'react'
 
 const Contact = () => {
   return (
     <>
-    <ContactInfo />
-    <Box id="contact" bg="blue.400" py={10}>
-      <Box textAlign="center" mb={6}>
-        <Heading size="lg" color="white">
-          Kontaktujte nás
-        </Heading>
-        <Text color="white" fontSize="sm">
-          Máte otázky? Napište nám pomocí formuláře níže.
-        </Text>
-      </Box>
+      <ContactInfoSection />
       <ContactForm />
-    </Box>
     </>
   )
 }
